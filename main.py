b69585172f2afe80a89e156fab7f99952b6aa510
geime <exmpl@>
2025-01-12 15:22:37 +0300

    created GridForge and Window classes

--- main.py
+++ main.py
@@ -1,25 +1,12 @@
 import pygame
+from mainFlow import GridForge
 
-DISPLAY_SIZE = DISPLAY_WIDTH, DISPLAY_HEIGHT = 600, 600
-FPS = 100
 ALL_SPRITES = pygame.sprite.Group()
 
 
 def main():
-    pygame.init()
-    clock = pygame.time.Clock()
-    screen = pygame.display.set_mode(DISPLAY_SIZE)
-
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-        pygame.display.flip()
-        clock.tick(FPS)
-
-    pygame.quit()
+    gf = GridForge()
+    gf.run_game()
 
 
 if __name__ == '__main__':
--- mainFlow.py
+++ mainFlow.py
@@ -1,3 +1,42 @@
-class BaseFlow:
-    def __init__(self):
+import pygame
+
+DISPLAY_SIZE = DISPLAY_WIDTH, DISPLAY_HEIGHT = 600, 600
+FPS = 100
+
+
+class Window:
+    def __init__(self, size, background=pygame.color.Color('black')):
+        self.surface = pygame.Surface(size)
+        if isinstance(background, pygame.color.Color):
+            self.surface.fill(background)
+        elif isinstance(background, pygame.sprite.Sprite):
+            pass
+            #  TODO заполнение спрайта по центру
+
+    def add_widget(self, widget, dist):
         pass
+
+
+class GridForge:
+    def __init__(self):
+        pygame.init()
+
+        self.clock = pygame.time.Clock()
+
+        self.screen = pygame.display.set_mode(DISPLAY_SIZE)
+        black_window = Window(DISPLAY_SIZE)
+        self.switch_window(black_window)
+
+    def run_game(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            pygame.display.flip()
+            self.clock.tick(FPS)
+
+    def switch_window(self, new_window):
+        self.current_window = new_window
+        self.screen.blit(new_window.surface, (0, 0))
