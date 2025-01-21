7a05880a268038dcaf721240c6e2b2ac2cb90d71
geime <exmpl@>
2025-01-21 18:37:07 +0300

    initial. Creation of MapUtilit and Field widget

---
+++ Widgets/Field.py
@@ -0,0 +1,5 @@
+from .widget import Widget
+class Field(Widget):
+    def __init__(self, rect, ccv_map):
+        super().__init__(rect)
+        pass
--- Windows/GameWindow.py
+++ Windows/GameWindow.py
@@ -1,5 +1,22 @@
 from .Window import Window
+import csv
+
+
+class MapUtilit:
+    def __init__(self):
+        self.map = []
+
+    def unpack_map(self, filename):
+        with open(filename, 'r', encoding='utf-8') as level_file:
+            reader = csv.reader(level_file, delimiter=';')
+            for i, row in enumerate(reader):
+                print(i, row)
+
 
 class GameWindow(Window):
     def __init__(self, parent):
         super().__init__(parent)
+        self.create_widgets()
+        self.add_background('darkgray')
+        mu = MapUtilit()
+        mu.unpack_map('level/level_0/map.csv')
